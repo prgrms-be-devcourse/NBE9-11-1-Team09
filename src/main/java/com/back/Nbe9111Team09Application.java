a2b1e7ed939d43d18707178951b3ea266ea73941
octuniv <hws090911@gmail.com>
2026-03-19 12:15:55 +0900

    First commit: 기존 프로젝트 연결

---
+++ src/main/java/com/back/Nbe9111Team09Application.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nbe9111Team09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nbe9111Team09Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/Nbe9111Team09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nbe9111Team09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
