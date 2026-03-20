129163e53fd2d3a332e05faaabfaa9a67a19b508
octuniv <hws090911@gmail.com>
2026-03-20 12:08:35 +0900

    add order controller and service

---
+++ src/main/java/com/back/domain/order/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.back.domain.order.controller;
+
+import com.back.domain.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/order")
+public class OrderController {
+    private final OrderService orderService;
+}
