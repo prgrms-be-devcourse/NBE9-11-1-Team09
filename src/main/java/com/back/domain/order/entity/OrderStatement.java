f72796a31e52d1b408eab83b367ddce5aeb3da2d
JW-1126 <jwyoon1126@naver.com>
2026-03-20 12:31:36 +0900

    feat(entity) : 생성자 수정 및 엔티티 매핑 편의 메서드 작성

--- src/main/java/com/back/domain/order/entity/OrderStatement.java
+++ src/main/java/com/back/domain/order/entity/OrderStatement.java
@@ -1,17 +1,23 @@
 package com.back.domain.order.entity;
 
 import com.back.global.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class OrderStatement extends BaseEntity {
     private String address;
     private String zipCode;
@@ -27,4 +33,10 @@ public class OrderStatement extends BaseEntity {
     @OneToMany(mappedBy = "orderStatement", cascade = CascadeType.ALL, orphanRemoval = true)
     @ToString.Exclude // toString 무한 루프 방지
     private List<OrderItem> orderItems = new ArrayList<>();
+
+    public OrderStatement(String address, String zipCode, Order order) {
+        this.address = address;
+        this.zipCode = zipCode;
+        this.order = order;
+    }
 }
