7eb8b9eb6a5c23ab83b2558e30dc406cb23694be
JW-1126 <jwyoon1126@naver.com>
2026-03-23 11:39:27 +0900

    feat(test): 정상 삭제 요청에 대한 테스트 작성

---
+++ src/test/java/com/back/domain/order/controller/OrderDeleteControllerTest.java
@@ -0,0 +1,102 @@
+package com.back.domain.order.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.back.domain.order.entity.CoffeeOrder;
+import com.back.domain.order.entity.OrderStatement;
+import com.back.domain.order.repository.OrderItemRepository;
+import com.back.domain.order.repository.OrderRepository;
+import com.back.domain.order.repository.OrderStatementRepository;
+import com.back.domain.product.entity.Product;
+import com.back.domain.product.repository.ProductRepository;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+class OrderDeleteControllerTest {
+
+    private static final int EXIST_ID = 1;
+    private static final int NOT_EXIST_ORDER_ID = 5;
+    private static final int NOX_EXIST_STATEMENT_ID = 5;
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private OrderStatementRepository orderStatementRepository;
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        for (int i = 1; i <= 4; i++) {
+            Product product = productRepository.findById(i).get();
+            CoffeeOrder order = new CoffeeOrder("test %d@naver.com".formatted(i));
+            OrderStatement statement = order.addOrderStatement("address" + i, "zipCord" + i);
+            statement.addOrderItem(i, product);
+
+            orderRepository.save(order);
+        }
+
+        em.flush();
+        em.clear();
+    }
+
+    // 정상적 입력에 대해 호출된 핸들러 & 응답 코드 확인 + db 정상 삭제 확인
+    @Test
+    @DisplayName("정상적인 삭제 요청에 대한 테스트")
+    void delete_t1() throws Exception {
+        //given
+        int orderId = EXIST_ID;
+        int statementId = EXIST_ID;
+        int itemId = EXIST_ID;
+
+        //when
+        ResultActions res = mockMvc.perform(
+                delete("/api/v1/order/%d/statement/%d".formatted(orderId, statementId))
+        );
+
+        em.flush();
+        em.clear();
+
+        //then
+        res.andExpect(handler().handlerType(OrderController.class))
+                .andExpect(handler().methodName("removeOrderStatement"))
+                .andExpect(status().is(204));
+
+        // order 삭제 확인
+        assertThat(orderRepository.findById(orderId)).isEmpty();
+        // statement 삭제 확인
+        assertThat(orderStatementRepository.findById(statementId)).isEmpty();
+        // orderItem 삭제 확인 (id=1이라고 가정)
+        assertThat(orderItemRepository.findById(itemId)).isEmpty();
+    }
+
+    // 오류에 대한 예외 처리 확인 ( 예외 종류 , 메세지 )
+
+    // order존재하지않음
+
+    // orderstatement존재하지않음
+}
