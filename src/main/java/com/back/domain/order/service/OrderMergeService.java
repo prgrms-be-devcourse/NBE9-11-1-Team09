807333dae8087a9af8a3db0cc3fc191c75af1a5b
5s-mini <dnclsehd123@naver.com>
2026-03-25 17:10:09 +0900

    feat: 주문 병합 로직이 부분적으로만 반영되어 있던 문제 해결

--- src/main/java/com/back/domain/order/service/OrderMergeService.java
+++ src/main/java/com/back/domain/order/service/OrderMergeService.java
@@ -2,7 +2,6 @@ package com.back.domain.order.service;
 
 import com.back.domain.order.dto.create.OrderCreateRequestDto;
 import com.back.domain.order.dto.merge.OrderMergeResponseDto;
-import com.back.domain.order.dto.update.OrderUpdateResponseDto;
 import com.back.domain.order.entity.CoffeeOrder;
 import com.back.domain.order.entity.OrderStatement;
 import com.back.domain.order.repository.OrderRepository;
@@ -15,20 +14,20 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class OrderMergeService {
 
-    private final OrderService orderService;
+    private final OrderService orderService; // 기존 서비스
     private final OrderRepository orderRepository;
 
     @Transactional
     public OrderMergeResponseDto createOrMergeOrderWithTotalAmount(OrderCreateRequestDto requestDto) {
 
-        OrderUpdateResponseDto basicResponse = orderService.createOrMergeOrder(requestDto);
+        orderService.createNewOrder(requestDto.email(), requestDto.orderStatements());
 
-        CoffeeOrder order = orderRepository.findByEmail(basicResponse.email())
+        CoffeeOrder order = orderRepository.findByEmail(requestDto.email())
                 .orElseThrow(() -> new IllegalArgumentException("주문을 찾을 수 없습니다."));
 
         List<OrderStatement> statements = order.getStatements();
         OrderStatement latestStatement = statements.get(statements.size() - 1);
 
-        return OrderMergeResponseDto.from(latestStatement, basicResponse.email());
+        return OrderMergeResponseDto.from(latestStatement, requestDto.email());
     }
 }
